dd7162e639be5b12f5e19d36d3ed704535b2ce25
Sathes Balasingam <sathes352@gmail.com>
2017-01-30 15:44:35 -0500

    5cats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,17 @@
 class PagesController < ApplicationController
+  before_action :set_kitten_url, only: [:kitten, :kittens]
 
+  def kitten
+  end
+
+  def kittens
+  end
+
+  def set_kitten_url
+    requested_size = params[:size]
+    @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
+  
     def welcome
       @header = "This is the header in the controller page"
       render :welcome
@@ -9,11 +21,6 @@ class PagesController < ApplicationController
     end
 
     def contest
-    end
-
-    def kitten
-      requested_size = params[:size]
-      @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
     end
 
 end
